5888370a15103350fd76a4f54aa64509314797ce
marin0v89 <xandarous89@gmail.com>
2021-07-06 23:11:30 +0300

    Hibernate code first homework.

---
+++ 03.HibernateCodeFirst/src/main/java/Entity/Sale.java
@@ -0,0 +1,56 @@
+package Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "sale")
+public class Sale extends CoreEntity {
+
+    @ManyToOne
+    private Product product;
+    @ManyToOne
+    private Customer customer;
+    @ManyToOne
+    private StoreLocation storeLocation;
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    public Sale() {
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public StoreLocation getStoreLocation() {
+        return storeLocation;
+    }
+
+    public void setStoreLocation(StoreLocation storeLocation) {
+        this.storeLocation = storeLocation;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+}
