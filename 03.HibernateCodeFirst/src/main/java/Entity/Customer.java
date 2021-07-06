9b4acfff61f400a144faf2887b8050854579fb9e
marin0v89 <xandarous89@gmail.com>
2021-07-06 23:11:20 +0300

    Hibernate code first homework.

---
+++ 03.HibernateCodeFirst/src/main/java/Entity/Customer.java
@@ -0,0 +1,61 @@
+package Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "customers")
+public class Customer extends CoreEntity {
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "credit_card_number", unique = true, nullable = false)
+    private String creditCardNumber;
+
+    @OneToMany(mappedBy = "customer")
+    private Set<Sale> sales;
+
+    public Set<Sale> getSales() {
+        return sales;
+    }
+
+    public void setSales(Set<Sale> sales) {
+        this.sales = sales;
+    }
+
+    public Customer() {
+        this.sales = new HashSet<Sale>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+}
