598b73268ae21b64135e697be957ca9a17e130c8
marin0v89 <xandarous89@gmail.com>
2021-07-06 23:11:25 +0300

    Hibernate code first homework.

---
+++ 03.HibernateCodeFirst/src/main/java/Entity/Product.java
@@ -0,0 +1,63 @@
+package Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "products")
+public class Product extends CoreEntity {
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String name;
+
+    @Column
+    private Integer quantity;
+
+
+    @Column(precision = 6, scale = 2)
+    private BigDecimal price;
+
+    @OneToMany(mappedBy = "product")
+    private Set<Sale> sales;
+
+    public Product() {
+        this.sales = new HashSet<Sale>();
+    }
+
+    public Set<Sale> getSales() {
+        return sales;
+    }
+
+    public void setSales(Set<Sale> sales) {
+        this.sales = sales;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
