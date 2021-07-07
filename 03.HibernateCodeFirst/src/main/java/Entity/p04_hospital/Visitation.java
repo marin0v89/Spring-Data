5d4801dd9e1d11a99ac2733f2590cf9384870673
marin0v89 <xandarous89@gmail.com>
2021-07-07 21:36:14 +0300

    Hibernate code first homework.

--- 03.HibernateCodeFirst/src/main/java/Entity/p04_hospital/Visitation.java
+++ 03.HibernateCodeFirst/src/main/java/Entity/p04_hospital/Visitation.java
@@ -1,48 +1,48 @@
-package Entity.p04_hospital;
-
-import Entity.CoreEntity;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import java.util.Date;
-
-@Entity(name = "visitations")
-public class Visitation extends CoreEntity {
-
-    @Column(nullable = false)
-    private Date date;
-
-    @Column(length = 100)
-    private String comments;
-
-    private Patient patient;
-
-
-    @ManyToOne
-    @JoinColumn(name = "patient_id", referencedColumnName = "id")
-    public Patient getPatient() {
-        return this.patient;
-    }
-
-    public void setPatient(Patient patient) {
-        this.patient = patient;
-    }
-
-    public Date getDate() {
-        return this.date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getComments() {
-        return this.comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-}
+//package Entity.p04_hospital;
+//
+//import Entity.CoreEntity;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.ManyToOne;
+//import java.util.Date;
+//
+//@Entity(name = "visitations")
+//public class Visitation extends CoreEntity {
+//
+//    @Column(nullable = false)
+//    private Date date;
+//
+//    @Column(length = 100)
+//    private String comments;
+//
+//    private Patient patient;
+//
+//
+//    @ManyToOne
+//    @JoinColumn(name = "patient_id", referencedColumnName = "id")
+//    public Patient getPatient() {
+//        return this.patient;
+//    }
+//
+//    public void setPatient(Patient patient) {
+//        this.patient = patient;
+//    }
+//
+//    public Date getDate() {
+//        return this.date;
+//    }
+//
+//    public void setDate(Date date) {
+//        this.date = date;
+//    }
+//
+//    public String getComments() {
+//        return this.comments;
+//    }
+//
+//    public void setComments(String comments) {
+//        this.comments = comments;
+//    }
+//}
