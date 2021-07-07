4fde4a8dbbecffb362559ad191310eee6f370050
marin0v89 <xandarous89@gmail.com>
2021-07-07 21:36:24 +0300

    Hibernate code first homework.

--- 03.HibernateCodeFirst/src/main/java/Entity/p04_hospital/Medicament.java
+++ 03.HibernateCodeFirst/src/main/java/Entity/p04_hospital/Medicament.java
@@ -1,34 +1,34 @@
-package Entity.p04_hospital;
-
-
-import Entity.CoreEntity;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
-import java.util.Set;
-
-@Entity(name = "medicaments")
-public class Medicament extends CoreEntity {
-    @Column(nullable = false, length = 40)
-    private String name;
-
-    private Set<Patient> patients;
-
-    @ManyToMany(mappedBy = "prescriptions")
-    public Set<Patient> getPatients() {
-        return this.patients;
-    }
-
-    public void setPatients(Set<Patient> patients) {
-        this.patients = patients;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
+//package Entity.p04_hospital;
+//
+//
+//import Entity.CoreEntity;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.ManyToMany;
+//import java.util.Set;
+//
+//@Entity(name = "medicaments")
+//public class Medicament extends CoreEntity {
+//    @Column(nullable = false, length = 40)
+//    private String name;
+//
+//    private Set<Patient> patients;
+//
+//    @ManyToMany(mappedBy = "prescriptions")
+//    public Set<Patient> getPatients() {
+//        return this.patients;
+//    }
+//
+//    public void setPatients(Set<Patient> patients) {
+//        this.patients = patients;
+//    }
+//
+//    public String getName() {
+//        return this.name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//}
