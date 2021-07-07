3662c9d8c9e05708142efcc5dea884a76438a910
marin0v89 <xandarous89@gmail.com>
2021-07-07 22:16:16 +0300

    Hibernate code first homework.

--- 03.HibernateCodeFirst/src/main/java/Entity/p03_uni/Teacher.java
+++ 03.HibernateCodeFirst/src/main/java/Entity/p03_uni/Teacher.java
@@ -2,17 +2,32 @@ package Entity.p03_uni;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 @Entity
 @Table(name = "teachers")
-public class Teacher extends CoreUser{
+public class Teacher extends CoreUser {
+
     @Column(unique = true, nullable = false)
     private String email;
+
     @Column(name = "salary_per_hour")
     private Integer salaryPerHour;
 
+    @OneToMany(mappedBy = "teacher")
+    private Set<Course> courses;
+
     public Teacher() {
+    }
+
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
     }
 
     public String getEmail() {
