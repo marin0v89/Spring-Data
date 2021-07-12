71aef86bfdb86562f620571e1c0263c0c71d5469
marin0v89 <xandarous89@gmail.com>
2021-07-13 01:11:16 +0300

    Homework

--- SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/CommandLineRunnerImpl.java
+++ SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/CommandLineRunnerImpl.java
@@ -1,7 +1,9 @@
 package erxs.spring.springintroexercise;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
