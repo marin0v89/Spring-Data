5e45359ac1c438a8a15fb802887411751f25e484
marin0v89 <xandarous89@gmail.com>
2021-07-12 18:26:38 +0300

    Spring introduction

---
+++ SpringIntro/src/main/java/spring/lab/springintro/models/User.java
@@ -0,0 +1,58 @@
+package spring.lab.springintro.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    private Integer age;
+
+    @OneToMany(mappedBy = "user")
+    private List<Account> accounts;
+
+    public User() {
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
