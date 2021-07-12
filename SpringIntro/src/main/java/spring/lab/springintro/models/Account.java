18ae10f048c6665205951754b5f3172e0fdda3ef
marin0v89 <xandarous89@gmail.com>
2021-07-12 18:26:44 +0300

    Spring introduction

---
+++ SpringIntro/src/main/java/spring/lab/springintro/models/Account.java
@@ -0,0 +1,47 @@
+package spring.lab.springintro.models;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private BigDecimal balance;
+
+    @ManyToOne()
+    private User user;
+
+
+    public Account() {
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
