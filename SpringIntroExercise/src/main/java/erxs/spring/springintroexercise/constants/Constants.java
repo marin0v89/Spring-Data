5086b3e9d3516b9451ac699dc1193c73e4abe35e
marin0v89 <xandarous89@gmail.com>
2021-07-13 23:26:07 +0300

    Homework

---
+++ SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/constants/Constants.java
@@ -0,0 +1,7 @@
+package erxs.spring.springintroexercise.constants;
+
+public class Constants {
+    public static final String AUTHORS_FILE_PATH = "src/main/resources/files/authors.txt";
+    public static final String BOOKS_FILE_PATH = "src/main/resources/files/books.txt";
+    public static final String CATEGORIES_FILE_PATH = "src/main/resources/files/categories.txt";
+}
