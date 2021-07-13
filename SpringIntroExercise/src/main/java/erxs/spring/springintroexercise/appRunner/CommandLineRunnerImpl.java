22d1f124c4fe2b408bead4788bd50056a67d2025
marin0v89 <xandarous89@gmail.com>
2021-07-13 23:26:11 +0300

    Homework

--- SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/appRunner/CommandLineRunnerImpl.java
+++ SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/appRunner/CommandLineRunnerImpl.java
@@ -1,12 +1,21 @@
 package erxs.spring.springintroexercise.appRunner;
 
+import erxs.spring.springintroexercise.service.CategoryService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
+
+    private final CategoryService categoryService;
+
+    public CommandLineRunnerImpl(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @Override
     public void run(String... args) throws Exception {
 
+        categoryService.seedCategories();
     }
 }
