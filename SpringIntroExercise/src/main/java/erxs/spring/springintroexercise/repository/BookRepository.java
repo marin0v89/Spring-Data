823753a88fd31a057282645960f3b4b2cd2082a0
marin0v89 <xandarous89@gmail.com>
2021-07-14 20:24:50 +0300

    Homework

--- SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/repository/BookRepository.java
+++ SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/repository/BookRepository.java
@@ -4,6 +4,15 @@ import erxs.spring.springintroexercise.models.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Service
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findAllByReleaseDateAfter(LocalDate releaseDate);
+
+    List<Book> findAllByReleaseDateBefore(LocalDate releaseDate);
+
+    List<Book> findAllByAuthor_FirstNameAndAuthor_LastNameOrderByReleaseDateDescTitle(String firstName, String lastName);
 }
