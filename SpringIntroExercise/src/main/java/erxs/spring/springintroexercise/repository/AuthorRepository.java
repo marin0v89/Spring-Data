30a5a48ff7210768585d56013ab477b534791add
marin0v89 <xandarous89@gmail.com>
2021-07-14 20:24:28 +0300

    Homework

--- SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/repository/AuthorRepository.java
+++ SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/repository/AuthorRepository.java
@@ -2,8 +2,14 @@ package erxs.spring.springintroexercise.repository;
 
 import erxs.spring.springintroexercise.models.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    @Query("SELECT a FROM Author a ORDER BY a.books.size DESC")
+    List<Author> findAllBooksDesc();
 }
