0f7188a5e3ded007f2d2643dfb08b18eb889f544
marin0v89 <xandarous89@gmail.com>
2021-07-14 20:24:35 +0300

    Homework

--- SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/service/BookService.java
+++ SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/service/BookService.java
@@ -1,7 +1,19 @@
 package erxs.spring.springintroexercise.service;
 
+import erxs.spring.springintroexercise.models.entity.Author;
+import erxs.spring.springintroexercise.models.entity.Book;
+
 import java.io.IOException;
+import java.util.List;
 
 public interface BookService {
     void seedBooks() throws IOException;
+
+    List<Book> findAllBooksAfterYear(int year);
+
+    List<String> findAllAuthorsAfterYear(int year);
+
+    List<String> findAllBooksByAuthor(String firstName,String lastName);
+
+
 }
