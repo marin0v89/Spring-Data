53cc1cc2c7245903774933d126194eea5cb7133e
marin0v89 <xandarous89@gmail.com>
2021-07-14 20:24:46 +0300

    Homework

--- SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/service/AuthorService.java
+++ SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/service/AuthorService.java
@@ -3,10 +3,12 @@ package erxs.spring.springintroexercise.service;
 import erxs.spring.springintroexercise.models.entity.Author;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface AuthorService {
     void seedAuthors() throws IOException;
 
     Author getRandomAuthor();
 
+    List<String> getAllAuthorsByBooks();
 }
