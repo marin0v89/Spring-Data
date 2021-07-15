579e299396c8a6083d1f7d39810e30c99e9ad8c6
marin0v89 <xandarous89@gmail.com>
2021-07-15 23:57:55 +0300

    Homework

--- SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/appRunner/CommandLineRunnerImpl.java
+++ SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/appRunner/CommandLineRunnerImpl.java
@@ -1,5 +1,6 @@
 package erxs.spring.springintroexercise.appRunner;
 
+import erxs.spring.springintroexercise.models.entity.AgeRestriction;
 import erxs.spring.springintroexercise.models.entity.Book;
 import erxs.spring.springintroexercise.service.AuthorService;
 import erxs.spring.springintroexercise.service.BookService;
@@ -7,7 +8,9 @@ import erxs.spring.springintroexercise.service.CategoryService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Locale;
 
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
@@ -15,11 +18,13 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
     private final CategoryService categoryService;
     private final AuthorService authorService;
     private final BookService bookService;
+    private final BufferedReader bufferedReader;
 
-    public CommandLineRunnerImpl(CategoryService categoryService, AuthorService authorService, BookService bookService) {
+    public CommandLineRunnerImpl(CategoryService categoryService, AuthorService authorService, BookService bookService, BufferedReader bufferedReader) {
         this.categoryService = categoryService;
         this.authorService = authorService;
         this.bookService = bookService;
+        this.bufferedReader = bufferedReader;
     }
 
     @Override
@@ -32,6 +37,21 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
 //        problemTwo(1990);
 //        problemThree();
 //        problemFour("George", "Powell");
+        System.out.println("Please select the problem number :");
+        int exNumber = Integer.parseInt(bufferedReader.readLine());
+
+        switch (exNumber) {
+            case 1 -> bookTitleByAgeRestriction();
+        }
+    }
+
+    private void bookTitleByAgeRestriction() throws IOException {
+        System.out.println("Please enter age restriction :");
+        AgeRestriction ageRestriction =
+                AgeRestriction.valueOf(bufferedReader.readLine().toUpperCase());
+
+        bookService.findAllByAgeRestriction(ageRestriction)
+                .forEach(book -> System.out.println(book.getTitle()));
     }
 
     private void problemFour(String firstName, String lastName) {
