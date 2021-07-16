e5acf09fbe28ea97d55a1c3fc9c0c8be879eb3a7
marin0v89 <xandarous89@gmail.com>
2021-07-16 17:48:20 +0300

    Homework

--- SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/repository/BookRepository.java
+++ SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/repository/BookRepository.java
@@ -3,7 +3,10 @@ package erxs.spring.springintroexercise.repository;
 import erxs.spring.springintroexercise.models.entity.AgeRestriction;
 import erxs.spring.springintroexercise.models.entity.Book;
 import erxs.spring.springintroexercise.models.entity.EditionType;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -27,4 +30,12 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findAllByReleaseDateBeforeOrReleaseDateAfter(LocalDate lower, LocalDate upper);
 
+    List<Book> findAllByTitleContaining(String contains);
+
+    List<Book> findAllByAuthor_LastNameStartsWith(String author_lastName);
+
+    @Query("SELECT COUNT(b) FROM Book b WHERE length(b.title) > :length ")
+    int countOfBooksWithTitleLengthMoreThan(@Param(value = "length") int length);
+
+    List<Book> findBookByTitle(String title);
 }
