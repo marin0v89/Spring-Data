cac969412a2412ea3cd10153ec7fb130ab28e467
marin0v89 <xandarous89@gmail.com>
2021-07-16 15:27:23 +0300

    Homework

--- SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/service/AuthorService.java
+++ SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/service/AuthorService.java
@@ -11,4 +11,6 @@ public interface AuthorService {
     Author getRandomAuthor();
 
     List<String> getAllAuthorsByBooks();
+
+    List<String> findAuthorsWhosFirstNameEndsWith(String endsWith);
 }
