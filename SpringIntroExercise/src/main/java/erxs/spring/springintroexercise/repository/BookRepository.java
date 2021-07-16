2d30c73e0d0b0a59c99d8a51bf49384ba942ca86
marin0v89 <xandarous89@gmail.com>
2021-07-16 13:08:13 +0300

    Homework

--- SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/repository/BookRepository.java
+++ SpringIntroExercise/src/main/java/erxs/spring/springintroexercise/repository/BookRepository.java
@@ -2,9 +2,11 @@ package erxs.spring.springintroexercise.repository;
 
 import erxs.spring.springintroexercise.models.entity.AgeRestriction;
 import erxs.spring.springintroexercise.models.entity.Book;
+import erxs.spring.springintroexercise.models.entity.EditionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -18,4 +20,10 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findAllByAuthor_FirstNameAndAuthor_LastNameOrderByReleaseDateDescTitle(String firstName, String lastName);
 
     List<Book> findAllByAgeRestriction(AgeRestriction ageRestriction);
+
+    List<Book> findAllByEditionTypeAndCopiesLessThan(EditionType editionType,Integer copies);
+
+    List<Book> findAllByPriceLessThanOrPriceGreaterThan(BigDecimal priceOne, BigDecimal priceTwo);
+
+    List<Book> findAllByReleaseDateBeforeOrReleaseDateAfter(LocalDate lower, LocalDate upper);
 }
