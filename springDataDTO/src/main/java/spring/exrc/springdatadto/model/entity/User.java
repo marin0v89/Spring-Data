37d4803626b014e200fa47a8e3aefaa5296ecd1b
marin0v89 <xandarous89@gmail.com>
2021-07-20 18:28:36 +0300

    HomeWork

---
+++ springDataDTO/src/main/java/spring/exrc/springdatadto/model/entity/User.java
@@ -0,0 +1,65 @@
+package spring.exrc.springdatadto.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity {
+    @Column(unique = true)
+    private String email;
+    @Column
+    private String password;
+    @Column(name = "full_name")
+    private String fullName;
+    @Column(name = "is_admin")
+    private Boolean isAdmin = false;
+    @ManyToMany
+    private Set<Game> games;
+
+    public User() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+
+    public Set<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(Set<Game> games) {
+        this.games = games;
+    }
+}
