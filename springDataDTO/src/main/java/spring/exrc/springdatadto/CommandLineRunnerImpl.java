a36ec50beadee7bf15ac2bb4b21a903beabd3351
marin0v89 <xandarous89@gmail.com>
2021-07-20 12:12:07 +0300

    Spring DTO

---
+++ springDataDTO/src/main/java/spring/exrc/springdatadto/CommandLineRunnerImpl.java
@@ -0,0 +1,12 @@
+package spring.exrc.springdatadto;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ springDataDTO/src/main/java/spring/exrc/springdatadto/SpringDataDtoApplication.java
@@ -0,0 +1,13 @@
+package spring.exrc.springdatadto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataDtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataDtoApplication.class, args);
+    }
+
+}
