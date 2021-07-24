dde42b50cbca79338ca94b7e7aa932ee7aa98a51
marin0v89 <xandarous89@gmail.com>
2021-07-24 14:21:30 +0300

    homework

--- JSON/src/main/java/json/exr/json/CommandLineRunnerImpl.java
+++ JSON/src/main/java/json/exr/json/CommandLineRunnerImpl.java
@@ -1,10 +1,26 @@
 package json.exr.json;
 
+import json.exr.json.service.CategoryService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
+@Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
+
+    private final CategoryService categoryService;
+
+    public CommandLineRunnerImpl(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @Override
     public void run(String... args) throws Exception {
+        seedData();
+    }
 
+    private void seedData() throws IOException {
+        categoryService.seedCategories();
     }
 }
