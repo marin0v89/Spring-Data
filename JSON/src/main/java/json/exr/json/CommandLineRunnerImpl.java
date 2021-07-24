668f6d80e21d72239c1f7fb8404431e8232dc48f
marin0v89 <xandarous89@gmail.com>
2021-07-24 10:38:12 +0300

    homework

---
+++ JSON/src/main/java/json/exr/json/CommandLineRunnerImpl.java
@@ -0,0 +1,10 @@
+package json.exr.json;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class CommandLineRunnerImpl implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
