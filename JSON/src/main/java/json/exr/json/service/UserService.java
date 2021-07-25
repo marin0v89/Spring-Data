1632433eebd2311a330a960606343bb2c53275cb
marin0v89 <xandarous89@gmail.com>
2021-07-25 13:12:42 +0300

    homework

--- JSON/src/main/java/json/exr/json/service/UserService.java
+++ JSON/src/main/java/json/exr/json/service/UserService.java
@@ -1,11 +1,15 @@
 package json.exr.json.service;
 
+import json.exr.json.model.dto.UserSoldDto;
 import json.exr.json.model.entity.Users;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface UserService {
     void seedUsers() throws IOException;
 
     Users findRandomUser();
+
+    List<UserSoldDto> findAllUsersWithMoreThanOneSell();
 }
