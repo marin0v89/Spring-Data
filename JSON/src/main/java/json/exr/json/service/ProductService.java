7bf72556a8c7e802d616128404edf4a852de3e5d
marin0v89 <xandarous89@gmail.com>
2021-07-25 13:14:15 +0300

    homework

--- JSON/src/main/java/json/exr/json/service/ProductService.java
+++ JSON/src/main/java/json/exr/json/service/ProductService.java
@@ -1,7 +1,13 @@
 package json.exr.json.service;
 
+import json.exr.json.model.dto.ProductNameAndPriceDto;
+
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface ProductService {
     void seedProducts() throws IOException;
+
+    List<ProductNameAndPriceDto> findAllProductsInRangeOrderByPrice(BigDecimal lower, BigDecimal upper);
 }
