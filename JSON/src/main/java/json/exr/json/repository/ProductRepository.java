37323def124a26905f0699f693c3148446dadd36
marin0v89 <xandarous89@gmail.com>
2021-07-25 13:13:13 +0300

    homework

--- JSON/src/main/java/json/exr/json/repository/ProductRepository.java
+++ JSON/src/main/java/json/exr/json/repository/ProductRepository.java
@@ -4,6 +4,11 @@ import json.exr.json.model.entity.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Products, Long> {
+
+    List<Products> findAllByPriceBetweenAndBuyerIsNullOrderByPriceDesc(BigDecimal lower, BigDecimal upper);
 }
