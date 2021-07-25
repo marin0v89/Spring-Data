2df0a2ee94961bd9d4606ac8f7134fbc61c3f2b0
marin0v89 <xandarous89@gmail.com>
2021-07-25 13:13:47 +0300

    homework

--- JSON/src/main/java/json/exr/json/repository/UserRepository.java
+++ JSON/src/main/java/json/exr/json/repository/UserRepository.java
@@ -2,6 +2,14 @@ package json.exr.json.repository;
 
 import json.exr.json.model.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
+
+    @Query("SELECT u FROM Users u " +
+            "WHERE (SELECT COUNT(p)FROM Products p WHERE p.seller.id = u.id)>0 " +
+            "ORDER BY u.lastName, u.firstName")
+    List<Users> findAllUsersWithMoreThanOneSoldProduct();
 }
